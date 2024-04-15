4a55ee9e783ae93e7fa3e3454138f57a923565d8
hyunw9 <rkdgusdnr32@naver.com>
2024-04-15 23:13:01 +0900

    feat : Application 추가

---
+++ assignment_2/src/main/java/org/sopt/sopt/SoptApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.sopt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoptApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SoptApplication.class, args);
+  }
+}
+
---
+++ assignment_2/src/test/java/org/sopt/sopt/SoptApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.sopt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
