99b779c9eab5afad8e41b6adbb89e21e120759e2
hyunw9 <rkdgusdnr32@naver.com>
2024-04-15 23:11:09 +0900

    feat : Repository 계층 추가

---
+++ assignment_2/src/main/java/org/sopt/sopt/member/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package org.sopt.sopt.member.repository;
+
+import org.sopt.sopt.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
