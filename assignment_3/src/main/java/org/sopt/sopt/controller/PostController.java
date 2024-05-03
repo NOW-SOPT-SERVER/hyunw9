4e680b1ca79a10068a958d87a1aab81564e2ac49
hyunw9 <rkdgusdnr32@naver.com>
2024-05-04 00:17:18 +0900

    refactor : 가독성을 위한 URI 매핑 변경

--- assignment_3/src/main/java/org/sopt/sopt/controller/PostController.java
+++ assignment_3/src/main/java/org/sopt/sopt/controller/PostController.java
@@ -3,48 +3,55 @@ package org.sopt.sopt.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.sopt.sopt.common.dto.request.PostCreateRequest;
-import org.sopt.sopt.common.dto.response.SuccessMessageResponse;
+import org.sopt.sopt.common.dto.response.responseEnum.SuccessMessage;
 import org.sopt.sopt.common.dto.response.SuccessStatusResponse;
 import org.sopt.sopt.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
+@Validated
 @RequiredArgsConstructor
 public class PostController {
 
   private final PostService postService;
 
-  @PostMapping("/api/v1/post")
+  @PostMapping("/post")
   public ResponseEntity<?> createArticle(
       @RequestHeader Long memberId,
       @RequestBody @Valid PostCreateRequest postCreateRequest) {
-    return ResponseEntity.status(HttpStatus.CREATED).body(SuccessStatusResponse.of(
-        SuccessMessageResponse.POST_WRITE_SUCCESS,postService.postArticle(memberId, postCreateRequest)
+    return ResponseEntity.status(HttpStatus.CREATED)
+        .body(SuccessStatusResponse.of(
+        SuccessMessage.POST_WRITE_SUCCESS,postService.postArticle(memberId, postCreateRequest)
     ));
   }
 
-  @GetMapping("/api/v1/post/all")
+  @GetMapping("/post/all")
   public ResponseEntity<?> getAllMemberPost(
       @RequestHeader Long memberId
   ) {
-    return ResponseEntity.status(HttpStatus.OK).body(SuccessStatusResponse.of(
-        SuccessMessageResponse.POST_GET_SUCCESS,postService.getMemberAllArticle(memberId)
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(SuccessStatusResponse.of(
+        SuccessMessage.POST_GET_SUCCESS,postService.getMemberAllArticle(memberId)
     ));
   }
 
-  @GetMapping("/api/v1/post/{postId}")
+  @GetMapping("/{postId}")
   public ResponseEntity<?> getSpecificPost(
       @PathVariable Long postId
   ){
-    return ResponseEntity.status(HttpStatus.OK).body(SuccessStatusResponse.of(
-        SuccessMessageResponse.POST_GET_SUCCESS,postService.getSpecificPost(postId)
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(SuccessStatusResponse.of(
+        SuccessMessage.POST_GET_SUCCESS,postService.getSpecificPost(postId)
     ));
   }
 }
